25bc349008876c8dfa8faa4d4a442d5ee91a5a01
Civermau <mausan.bedolla@gmail.com>
2025-05-04 23:53:25 -0600

    Refactor command handling in main.cpp
    Replaced the previous command searching method from ifs statements to a dictionary using an unordered_map.
    Avoids the program having to go through multiple ifs to just search for the entry using the precommand as the key value, giving it a search time of O(1) instead of a linear search time. This also adds a case if there is no key found; therefore, that command does not exist.
    
    Also improves maintainability and scalability, making it easier to implement future commands.

--- FileSystem-NMT/main.cpp
+++ FileSystem-NMT/main.cpp
@@ -1,69 +1,109 @@
 #include <iostream>
 #include "Folder.h"
 #include "Display.h"
+#include <unordered_map>
+#include <functional>
+#include <string>
+
+// Method prototype for parsing command and arguments
+std::pair<std::string, std::string> parseCommand(const std::string& input);
+
 int main() {
 	std::shared_ptr<Folder> root = std::make_shared<Folder>("~");
 	std::shared_ptr<Folder> ALIVE = root;
-	while (true) {
+	
+	// Define command handlers
+	std::unordered_map<std::string, std::function<void(const std::string&)>> commands;
+	bool running = true;
+	
+	// Register all commands
+	commands["clear"] = [](const std::string&) {
+		dsp::ClearConsole();
+	};
+	
+	commands["ls"] = [&root](const std::string&) {
+		root->ShowContents(dsp::StandarDisplay);
+	};
+	
+	commands["tree"] = [&root](const std::string&) {
+		root->ShowAll(dsp::StandarDisplay, 0);
+	};
+	
+	commands["exit"] = [&running](const std::string&) {
+		running = false;
+	};
+	
+	commands["rm"] = [&root](const std::string& arg) {
+		if (!root->DeleteContent(arg)) {
+			dsp::StandarDisplay("bash: rm: " + arg + ": Doesn't exists\n");
+		}
+	};
+	
+	commands["cd"] = [&root, &ALIVE](const std::string& arg) {
+		if (arg == "..") {
+			root = root->GetParent();
+			return;
+		}
+		if (arg == "~") {
+			root = ALIVE;
+			return;
+		}
+		if (root->GetSubFolder(arg).expired()) {
+			dsp::StandarDisplay("bash: cd: " + arg + ": No such file or directory\n");
+			return;
+		}
+		root = root->GetSubFolder(arg).lock();
+	};
+	
+	commands["mkdir"] = [&root](const std::string& arg) {
+		if (!root->CreateContent(arg, NodeType::NodeFolder)) {
+			dsp::StandarDisplay("bash: mkdir: " + arg + ": Directory already exists\n");
+		}
+	};
+	
+	commands["touch"] = [&root](const std::string& arg) {
+		if (!root->CreateContent(arg, NodeType::NodeFile)) {
+			dsp::StandarDisplay("bash: touch: " + arg + ": File already exists\n");
+		}
+	};
+	
+	// Main command processing loop
+	while (running) {
+		// Get command input from user
 		dsp::StandarDisplay(root->GetPath() + "$ ");
-		std::string command = dsp::GetInput();
-		std::string preCmd = dsp::GetCMD(command);
+		std::string input = dsp::GetInput();
 		
-		if (preCmd == "clear") {
-			dsp::ClearConsole();
-			continue;
-		}
+		// Parse command and arguments
+		std::string preCmd, command;
+		std::tie(preCmd, command) = parseCommand(input);
 		
-		if (preCmd == "ls") {
-			root->ShowContents(dsp::StandarDisplay);
-		}
-
-		if (preCmd == "tree") {
-			root->ShowAll(dsp::StandarDisplay, 0);
-		}
-
-		if (preCmd == "close") {
-			break;
-		}
-
-		if (command == "\0") {
+		// Skip empty commands
+		if (preCmd.empty()) {
 			continue;
-		}
-
-		if (preCmd == "rm") {
-			if (!root->DeleteContent(command)) {
-				dsp::StandarDisplay("bash: rm: " + command + ": Doesn't exists\n");
-			}
 		}
 		
-
-		if (preCmd == "cd") {
-			if (command == "..") {
-				root = root->GetParent();
-				continue;
-			}
-			if (command == "~") {
-				root = ALIVE;
-				continue;
-			}
-			if (root->GetSubFolder(command).expired()) {
-				dsp::StandarDisplay("bash: cd:  " + command + ": No such file or directory\n");
-				continue;
-			}
-			root = root->GetSubFolder(command).lock();
-			continue;
-		}
-		if (preCmd == "mkdir") {
-			if (!root->CreateContent(command, NodeType::NodeFolder)) {
-				dsp::StandarDisplay("bash: mkdir:  " + command + ": Directory already exists\n");
-			}
-			continue;
-		}
-		if (preCmd == "touch") {
-			if (!root->CreateContent(command, NodeType::NodeFile)) {
-				dsp::StandarDisplay("bash: touch:  " + command + ": File already exists\n");
-			}
-			continue;
+		// Execute command if it exists
+		auto cmdIt = commands.find(preCmd);
+		if (cmdIt != commands.end()) {
+			cmdIt->second(command);
+		} else {
+			dsp::StandarDisplay("Unknown command: " + preCmd + "\n");
 		}
 	}
+}
+
+// Helper function to parse command and arguments
+std::pair<std::string, std::string> parseCommand(const std::string& input) {
+	std::string cmd, arg;
+	size_t spacePos = input.find(' ');
+	
+	if (spacePos != std::string::npos) {
+		cmd = input.substr(0, spacePos);
+		arg = input.substr(spacePos + 1);
+	} else {
+		cmd = input;
+		arg = "";
+	}
+	
+	return {cmd, arg};
 }
